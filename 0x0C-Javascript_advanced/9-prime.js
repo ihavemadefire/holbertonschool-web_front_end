793b470c2cd147b688cf5d83cb7793855e2fcf42
Jacob Ide <jacobmeide@gmail.com>
2021-02-18 12:11:30 -0600

    Initial attempt

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,19 @@
+function countPrimeNumbers() {
+    let count = 1;
+    let prime;
+    for (let i = 2; i < 101; i++) {
+        for (let j = 2; j < 101; j++) {
+            if ( i % j === 0)
+                count++;
+        }
+    }
+    return count;
+}
+
+time1 = performance.now();
+countPrimeNumbers();
+time2 = performance.now();
+let totalTime = time2 - time1;
+console.log("Execution time of printing countPrimeNumbers was " +
+totalTime +
+" milliseconds.");
